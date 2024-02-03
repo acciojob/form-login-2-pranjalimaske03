38fed3c35e69b745b9167fd829ca6489e4209821
Pranjali Maske <110550905+pranjalimaske03@users.noreply.github.com>
2024-02-03 08:56:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+//your JS code here. If required.
+ function fun(){
+            let firstName = document.getElementById("firstName");
+            let lastName = document.getElementById("lastName");
+            let phone = document.getElementById("phone");
+            let email = document.getElementById("email");
+            
+            alert(`
+            First Name : ${firstName.value}
+            Last Name : ${lastName.value}
+            Phone Number : ${phone.value}
+            Email ID : ${email.value}
+            ` )
+        }
+        let btn=document.getElementById("submit");
+        btn.addEventListener("click",fun)
